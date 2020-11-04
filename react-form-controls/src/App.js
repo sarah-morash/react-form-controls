b69f8aea2d6040648217e766dfa232c05d34dcda
Sarah Morash <sarahjmorash@gmail.com>
2020-11-04 09:49:34 -0400

    Add React hook forms and copy first example

---
+++ react-form-controls/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="example" defaultValue="test" ref={register} />
+      <input name="exampleRequired" ref={register({ required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+}
